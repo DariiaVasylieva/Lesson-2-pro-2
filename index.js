bc190779e4f3fbc22668941c0657a7dbba4ee767
DariiaVasylieva <vasil.dasha04@gmail.com>
2023-04-26 22:06:31 +0200

    finished work

---
+++ index.js
@@ -0,0 +1,12 @@
+firstNumber = +prompt('Введите первое число:');
+secondNumber = +prompt('Введите второе число:');
+
+sum = firstNumber + secondNumber;
+minus = firstNumber - secondNumber;
+div = firstNumber / secondNumber;
+mult = firstNumber * secondNumber;
+
+alert(`Сумма: ${sum} 
+Разница: ${minus} 
+Деление: ${div} 
+Умножение: ${mult}`);
